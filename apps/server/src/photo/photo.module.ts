505fb65de09900858dcebfd6d686376a771e3ba2
Lee Seunghyeon <77599531+subir-sh@users.noreply.github.com>
2026-09-07 00:03:20 +0900

    feat: wire photo import feature

--- apps/server/src/photo/photo.module.ts
+++ apps/server/src/photo/photo.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PhotoController } from './photo.controller';
 import { Photo } from './photo.entity';
+import { PhotoService } from './photo.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Photo])],
-  exports: [TypeOrmModule],
+  controllers: [PhotoController],
+  providers: [PhotoService],
+  exports: [PhotoService],
 })
 export class PhotoModule {}
