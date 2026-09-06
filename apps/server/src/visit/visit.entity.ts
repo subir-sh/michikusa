ab25b3fbb53c689ee344474f489bb24d28bc497c
Lee Seunghyeon <77599531+subir-sh@users.noreply.github.com>
2026-09-07 01:39:26 +0900

    refactor: simplify visit entity

--- apps/server/src/visit/visit.entity.ts
+++ apps/server/src/visit/visit.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Photo } from '../photo/photo.entity';
 import { Place } from '../place/place.entity';
 
@@ -7,17 +14,18 @@ export class Visit {
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @Column()
+  placeId!: number;
+
   @ManyToOne(() => Place, (place) => place.visits, {
     nullable: false,
     onDelete: 'CASCADE',
   })
+  @JoinColumn({ name: 'placeId' })
   place!: Place;
 
   @Column({ type: 'datetime' })
   visitedAt!: Date;
-
-  @Column({ default: false })
-  confirmed!: boolean;
 
   @OneToMany(() => Photo, (photo) => photo.visit)
   photos!: Photo[];
