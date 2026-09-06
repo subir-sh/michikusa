9e3f1ca72c38c35bc5f646af3937096b1ff9a9eb
Lee Seunghyeon <77599531+subir-sh@users.noreply.github.com>
2026-09-07 01:39:57 +0900

    feat: provide visit grouping service

--- apps/server/src/visit/visit.module.ts
+++ apps/server/src/visit/visit.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Visit } from './visit.entity';
+import { VisitService } from './visit.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Visit])],
-  exports: [TypeOrmModule],
+  providers: [VisitService],
+  exports: [VisitService],
 })
 export class VisitModule {}
