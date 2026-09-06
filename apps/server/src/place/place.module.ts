ae1554318df529cdbe82f603bf30015d1d578ca1
Lee Seunghyeon <77599531+subir-sh@users.noreply.github.com>
2026-09-07 01:40:09 +0900

    feat: wire place confirmation to visits

--- apps/server/src/place/place.module.ts
+++ apps/server/src/place/place.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PhotoModule } from '../photo/photo.module';
+import { VisitModule } from '../visit/visit.module';
 import { PlaceController } from './place.controller';
 import { Place } from './place.entity';
 import { PlaceService } from './place.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Place]), PhotoModule],
+  imports: [TypeOrmModule.forFeature([Place]), PhotoModule, VisitModule],
   controllers: [PlaceController],
   providers: [PlaceService],
   exports: [TypeOrmModule],
