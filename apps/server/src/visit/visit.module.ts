afb443a89b0fe8b5215f60b2f1f6f514aca40e8d
Lee Seunghyeon <77599531+subir-sh@users.noreply.github.com>
2026-09-07 01:49:39 +0900

    feat: expose visit queries

--- apps/server/src/visit/visit.module.ts
+++ apps/server/src/visit/visit.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { VisitController } from './visit.controller';
 import { Visit } from './visit.entity';
 import { VisitService } from './visit.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Visit])],
+  controllers: [VisitController],
   providers: [VisitService],
   exports: [VisitService],
 })
